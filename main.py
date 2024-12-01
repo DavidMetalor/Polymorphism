97e5e72b160cd9234021bca3dbeea02e1703daad
DavidMetalor <davidmetalor46@gmail.com>
2024-12-01 19:32:16 +0100

    Polymorphism

---
+++ main.py
@@ -0,0 +1,22 @@
+class eagle: 
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+    def info(self):
+        print(self.name)
+        print(self.age)
+    def make_sound(self):
+        print("whistle")
+
+class hawk:
+   def __init__(self, name, age):
+        self.name=name
+        self.age=age
+   def info(self):
+        print(self.name)
+        print(self.age)
+   def make_sound(self): 
+       print("call")
+
+obj1=eagle("task",2)
+obj2=hawk("kit",1)
